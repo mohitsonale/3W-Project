86cf62e4ea85e4d64476c5bc535d1c08f906120a
Mohit Sonale <mohitsonale@gmail.com>
2026-02-07 23:34:58 +0530

    3W

--- 3W_PROJECT/frontend/3W-project/src/api.jsx
+++ 3W_PROJECT/frontend/3W-project/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://threew-project.onrender.com"
+  baseURL: "https://threew-project.onrender.com/api"
 });
 
 export default API;
